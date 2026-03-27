19b91ac01764ed6d59a28098c70fdaba01275e4c
truongdt24 <trongtruong0209@gmail.com>
2026-03-27 13:55:05 +0700

    add dockerfile and docker compose

--- src/test/java/com/truong/onlinebank/OnlineBankApplicationTests.java
+++ src/test/java/com/truong/onlinebank/OnlineBankApplicationTests.java
@@ -1,13 +1,10 @@
 package com.truong.onlinebank;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OnlineBankApplicationTests {
-
     @Test
     void contextLoads() {
+        // basic test
     }
-
 }
